de2b971a6d16349e0f2a2e2c5df0b04f44fb23b3
lucasventura01 <lnuceteventura@udesa.edu.ar>
2025-09-21 13:24:59 -0300

    creacion de archivos controller

--- myapp/routes/productos.js
+++ myapp/routes/productos.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productosController = require('../controllers/productosController.js');
 
 
-router.get('/:idProducto', function(req,res) {
-    res.send("Bienvenidos al detalle del producto " + req.params.idProducto);
-});
+router.get('/:idProducto', productosController.detalle);
 
-router.get('/:idProducto/comentarios/:idComentario?', function(req,res) {
-    if (req.params.idComentario == undefined) {
-        res.send("Bienvenidos a los comentarios del producto " + req.params.idProducto);
-    } else {
-        res.send("Bienvenidos a los comentarios del producto " + req.params.idProducto + " y estas enfocado en el comentario " + req.params.idComentario);
-    }
-});
+router.get('/:idProducto/comentarios/:idComentario?', productosController.detalleComentarios );
 
 
 module.exports = router;
