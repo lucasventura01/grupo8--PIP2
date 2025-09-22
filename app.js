13598f749607b4719ee4ff236e4389dd44b54091
lucasventura01 <lnuceteventura@udesa.edu.ar>
2025-09-22 15:25:18 -0300

    arreglos en las rutas

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
 // app.listen(3000, () => console.log('esto fue exitoso'));
 
-var rutasMain = require('./routes/main.js');
-var rutasUsers = require('./routes/users.js');
-let rutasProductos = require('./routes/productos.js')
-let rutasRegistro = require('./routes/registro.js')
-let rutasLogin = require('./routes/login.js')
+let rutasMain = require('./myapp/routes/main.js');
+let rutasUsers = require('./myapp/routes/users.js');
+let rutasProductos = require('./myapp/routes/productos.js')
+let rutasRegistro = require('./myapp/routes/registro.js')
+let rutasLogin = require('./myapp/routes/login.js')
 
 app.use('/productos', rutasProductos);
 app.use('/registro', rutasRegistro);
@@ -18,7 +19,6 @@ app.use('/login', rutasLogin);
 app.use('/main', rutasMain);
 app.use('/users', rutasUsers);
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,8 +30,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,5 +46,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
