e6056119225a4cc72c0d49ad8f6e3b5bb122991d
Mateo Carballido <mcarballido@udesa.edu.ar>
2025-11-10 15:43:20 -0300

    correccion en el buscador

--- myapp/controllers/mainController.js
+++ myapp/controllers/mainController.js
@@ -5,24 +5,28 @@ const op = db.Sequelize.Op;
 const mainController = {
     index: function (req, res) {
         db.Product.findAll({
-            include: [{association: "user"}, {association: "comment"}],
+            include: [{ association: "user" }, { association: "comment" }],
         })
-        .then(function (products) {
-            res.render('index', {productos: products});
-        })
-        .catch(function (error) {
-            res.send(error);
-        });
+            .then(function (products) {
+                res.render('index', { productos: products });
+            })
+            .catch(function (error) {
+                res.send(error);
+            });
 
     },
 
     search: function (req, res) {
         let criterio = req.query.search;
+
         db.Product.findAll({
             where: {
-                nombre_producto: { [op.like]: `%${criterio}%` },
-                include: [{ association: "user" }, { association: "comment", include: [{ association: "user" }] }]
-            }
+                nombre_producto: { [op.like]: '%'+ criterio + '%' }
+            },
+            include: [
+                { association: "user" },
+                { association: "comment", include: [{ association: "user" }] }
+            ]
         })
             .then(function (products) {
                 return res.render("search-results", { products: products, criterio: criterio });
@@ -31,6 +35,7 @@ const mainController = {
                 console.log(error);
             });
     }
+
 };
 
 module.exports = mainController;
